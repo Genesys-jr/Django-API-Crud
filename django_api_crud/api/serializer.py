f7223ea08836d84551fd8261fa0a9f39aa2233e2
Amon Daniel <amon.daniel.91@gmail.com>
2024-07-26 17:59:49 +0000

    comments

--- django_api_crud/api/serializer.py
+++ django_api_crud/api/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import User
 
+
+# returns the data in json format
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
--- django_api_crud/api/urls.py
+++ django_api_crud/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import get_users, create_user, user_detail
 
-
+# url paths
 urlpatterns = [
   path('users/' , get_users, name='get_users'),
   path('users/create/' , create_user, name='create_user'),
